a03002b4d6347036d1bbdc93e392f7aa74c0bcf2
Dharma41 <dharmendrareddy2000@gmail.com>
2024-09-14 19:29:27 -0500

    Css files updated

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-// import About from'./About';
+import '../CSS/Header.css';
 
 function Header() {
   return (
     <header className="Header">
-    <div className="about-background_header"></div> 
-     <h1>Dharmendra Reddy Chitte</h1>
+      <div className="about-background_header"></div> 
+      <h1>Dharmendra Reddy Chitte</h1>
       <nav>
         <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about" className="nav-button">About</a></li>
+          <li><a href="#projects" className="nav-button">Projects</a></li>
+          <li><a href="#contact" className="nav-button">Contact</a></li>
         </ul>
       </nav>
-     
     </header>
   );
 }
