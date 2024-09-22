e66bc22ac20f2679de6ef2448c2e8d0b8a760348
Dharma41 <dharmendrareddy2000@gmail.com>
2024-09-21 20:48:32 -0500

    GPA Added in eductioan(About.jsx)

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -26,12 +26,15 @@ const About = () => {
           I am a Full Stack Developer passionate about learning, quick to adapt, and motivated to solve problems. I have a strong foundation in full-stack development and am eager to contribute to the industry.          </p>
           <h2 className="education-title">Education</h2>
           <div className="education-item">
-            <div className="education-details">
-              <h3>Master in Computer Science</h3>
-              <span className="education-year">2023-2025</span>
-            </div>
-            <p>Oklahoma State University</p>
-          </div>
+  <div className="education-details">
+    <h3>Master in Computer Science</h3>
+    <span className="education-year">2023-2025</span>
+  </div>
+  <p>
+    Oklahoma State University
+    <span className="gpa">GPA: 4.0</span> {/* Add GPA here */}
+  </p>
+</div>
           <h2 className="skills-title">Skills</h2>
           <div className="skills-grid">
             <div className="skill-item">
